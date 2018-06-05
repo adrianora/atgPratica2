3d3d2854c8c51ebc5b1ef8904a0a05e57db6646c
Igordl <igor.lucena@ccc.ufcg.edu.br>
2018-06-05 09:02:10 -0300

    Adição de testes na classe GrafoComArestasNegativas

--- src/p2/GrafoComArestasNegativas.java
+++ src/p2/GrafoComArestasNegativas.java
@@ -1,12 +1,14 @@
 package p2;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.text.DecimalFormat;
 
 import org.junit.Test;
 
 import grafo.Graph;
+import grafo.Vertice;
 import main.GraphLib;
 
 public class GrafoComArestasNegativas {
@@ -49,6 +51,70 @@ public class GrafoComArestasNegativas {
 		
 		assertEquals(true, novo.connected(graph));
 		
+	}
+	
+	@Test
+	public void testGraphRepresentation() {
+		
+		String saida1 = "0 1 2 4\n"
+				+ "1 0 -2 0 0\n"
+				+ "2 -2 0 -7 0\n"
+				+ "3 0 -7 0 1\n"
+				+ "4 0 0 1 0\n";	
+		
+
+		//assertTrue(saida1.equals(novo.graphRepresentation(graph, "AM")));
+	
+	}
+	
+	@Test 
+	public void testGraphRepresentation2() {
+		
+		String saida2 = "1 - 2(-2)\n"
+				+ "2 - 1(-2) 3(-7)\n"
+				+ "3 - 2(-7) 4(1)\n"
+				+ "4 - 3(1)\n";
+		
+		
+		//assertTrue(saida2.equals(novo.graphRepresentation(graph, "AL")));
+	}
+	
+	@Test
+	public void testBFS() {
+		
+		String tree = "1 - 0 -\n"
+				+ "2 - 1 1\n"
+				+ "3 - 2 2\n"
+				+ "4 - 3 3\n";
+		
+		//assertTrue(tree.equals(novo.BFS(graph, 1)));
+	}
+	
+	@Test
+	public void testDFS() {
+		
+		String tree2 = "1 - 0 -\n"
+				+ "2 - 1 1\n"
+				+ "3 - 2 2\n"
+				+ "4 - 3 3\n";
+		
+		
+		//assertTrue(tree2.equals(novo.DFS(graph, 1)));
+	
+	}
+	
+	@Test
+	public void testShortestPath() {
+		
+		String shortestPath = "1 2 3";
+		String shortestPath2 = "1 2 3 4";
+		
+		Vertice v = new Vertice(1);
+		Vertice vFinal = new Vertice(3);
+		Vertice vFinal2 = new Vertice(4);
+		
+		assertTrue(shortestPath.equals(graph.shortestPath(v, vFinal)));
+		assertTrue(shortestPath2.equals(graph.shortestPath(v, vFinal2)));
 	}
 }
 
